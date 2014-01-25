30476ff076522f02ea3be0c8e13cd7a59a75a25c
Feminist-Software-Foundation <feministsoftwarefoundation@loves.dicksinmyan.us>
2014-01-25 21:27:48 +0000

    Added loads, categorised

--- internet-plus-equality.user.js
+++ internet-plus-equality.user.js
@@ -44,9 +44,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
+        //Core words
         v = v.replace(/(P)enis/gi, function(str, p1) {
             r = (p1 == "P") ? "R" : "r";
-            return r+"od of oppression";
+            return r+"ape instrument";
         });
         v = v.replace(/(R)ape/gi, function(str, p1) {
             T = (p1 == "R") ? "T" : "T";
@@ -60,6 +61,12 @@ function handleText(textNode)
             f = (p1 == "C") ? "F" : "f";
             return f+"eels";
         });
+        v = v.replace(/\b(M)erit\b/gi, function(str, p1) {
+            w = (p1 == "M") ? "W" : "w";
+            return w+"hite privilege";
+        });
+
+        //The Masculine
         v = v.replace(/\b(M)an\b/gi, function(str, p1) {
             r = (p1 == "M") ? "R" : "r";
             return r+"apist scum";
@@ -69,18 +76,62 @@ function handleText(textNode)
             return t+"he Patriarchy";
         });
         v = v.replace(/\b(B)oy\b/gi, function(str, p1) {
-            p = (p1 == "B") ? "P" : "p";
-            return p+"otential rapist";
+            f = (p1 == "B") ? "F" : "f";
+            return f+"uture rapist";
         });
         v = v.replace(/\b(M)ale\b/gi, function(str, p1) {
             s = (p1 == "M") ? "S" : "s";
             return s+"exist and misogynistic";
         });
+
+        //The Feminine
+        v = v.replace(/\b(B)itch\b/gi, function(str, p1) {
+            s = (p1 == "B") ? "S" : "s";
+            return s+"trong Womyn";
+        });
+
+        //Body Acceptance
+        v = v.replace(/\b(U)gly\b/gi, function(str, p1) {
+            b = (p1 == "U") ? "B" : "b";
+            return b+"eautiful";
+        });
+        v = v.replace(/\b(F)at\b/gi, function(str, p1) {
+            b = (p1 == "F") ? "B" : "b";
+            return b+"eautifully proportioned";
+        });
+        v = v.replace(/\b(D)iet\b/gi, function(str, p1) {
+            a = (p1 == "D") ? "A" : "a";
+            return a+"norexia";
+        });
+        v = v.replace(/\b(H)ealthy\b/gi, function(str, p1) {
+            f = (p1 == "H") ? "F" : "f";
+            return f+"at shaming";
+        });
+        v = v.replace(/\b(D)octor\b/gi, function(str, p1) {
+            f = (p1 == "D") ? "F" : "f";
+            return f+"at shamer";
+        });
+        v = v.replace(/\b(E)xercise\b/gi, function(str, p1) {
+            t = (p1 == "E") ? "T" : "t";
+            return t+"orture";
+        });
+        v = v.replace(/\b(R)unning\b/gi, function(str, p1) {
+            d = (p1 == "R") ? "D" : "d";
+            return d+"eath marching";
+        });
+        v = v.replace(/\b(O)bese\b/gi, function(str, p1) {
+            t = (p1 == "O") ? "T" : "t";
+            return t+"hyroid condition";
+        });
+
+        //Ideologies
         v = v.replace(/(E)galitarian/gi, function(str, p1) {
             r = (p1 == "E") ? "R" : "r";
             return r+"ape apologist";
         });
 	v = v.replace(/\bMRA\b/gi, "VIRGIN NECKBEARD");
+
+        //Sexualities and Gender Identities
         v = v.replace(/(H)eterosexual/gi, function(str, p1) {
             p = (p1 == "H") ? "P" : "p";
             return p+"rivileged scum";
@@ -104,6 +155,16 @@ function handleText(textNode)
         v = v.replace(/(T)ranssexual/gi, function(str, p1) {
             g = (p1 == "T") ? "G" : "g";
             return g+"ender traitor";
+        });
+
+        //Political parties
+	v = v.replace(/\bRepublican\b/gi, "Nazi");
+
+
+        //Race
+        v = v.replace(/(B)lack/gi, function(str, p1) {
+            n = (p1 == "B") ? "N" : "n";
+            return n+"oble race";
         });
 	
 	textNode.nodeValue = v;
