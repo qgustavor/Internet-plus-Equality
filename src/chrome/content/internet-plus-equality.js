7bd693273acfb2fea30ed8ac5521f4cec73972e5
Feminist-Software-Foundation <feministsoftwarefoundation@loves.dicksinmyan.us>
2014-01-25 19:00:57 +0000

    Fix script, now it works

--- src/chrome/content/internet-plus-equality.js
+++ src/chrome/content/internet-plus-equality.js
@@ -32,39 +32,39 @@ function handleText(textNode)
 	var v = textNode.nodeValue;
 
         v = v.replace(/(P)enis/gi, function(str, p1) {
-            b = (p1 == "P") ? "R" : "r";
+            r = (p1 == "P") ? "R" : "r";
             return r+"od of oppression";
         });
         v = v.replace(/(R)ape/gi, function(str, p1) {
-            b = (p1 == "R") ? "T" : "T";
+            T = (p1 == "R") ? "T" : "T";
             return T+"RIGGER WARNING: RAPE";
         });
         v = v.replace(/(M)an/gi, function(str, p1) {
-            b = (p1 == "M") ? "R" : "r";
+            r = (p1 == "M") ? "R" : "r";
             return r+"apist scum";
         });
         v = v.replace(/(M)en/gi, function(str, p1) {
-            b = (p1 == "M") ? "T" : "t";
+            t = (p1 == "M") ? "T" : "t";
             return t+"he Patriarchy";
         });
         v = v.replace(/(M)ale/gi, function(str, p1) {
-            b = (p1 == "M") ? "S" : "s";
+            s = (p1 == "M") ? "S" : "s";
             return s+"exist and misogynistic";
         });
         v = v.replace(/(E)galitarian/gi, function(str, p1) {
-            b = (p1 == "E") ? "R" : "r";
+            r = (p1 == "E") ? "R" : "r";
             return r+"ape apologist";
         });
         v = v.replace(/(H)eterosexual/gi, function(str, p1) {
-            b = (p1 == "H") ? "P" : "p";
+            p = (p1 == "H") ? "P" : "p";
             return p+"rivileged scum";
         });
         v = v.replace(/(S)traight/gi, function(str, p1) {
-            b = (p1 == "S") ? "O" : "o";
+            o = (p1 == "S") ? "O" : "o";
             return o+"ppressive to sexual minorities";
         });
         v = v.replace(/(G)ay/gi, function(str, p1) {
-            b = (p1 == "G") ? "M" : "m";
+            m = (p1 == "G") ? "M" : "m";
             return m+"em with so much internalised misogyny that they'd rather sleep with men";
         });
 	v = v.replace(/\bF2M\b/g, "Enabler of Patriarchy");
@@ -72,11 +72,11 @@ function handleText(textNode)
 	v = v.replace(/\bM2F\b/g, "Fake women");
 	v = v.replace(/\bm2f\b/g, "fake women");
         v = v.replace(/(T)ransgender/gi, function(str, p1) {
-            b = (p1 == "T") ? "G" : "g";
+            g = (p1 == "T") ? "G" : "g";
             return g+"ender traitor";
         });
         v = v.replace(/(T)ranssexual/gi, function(str, p1) {
-            b = (p1 == "T") ? "G" : "g";
+            g = (p1 == "T") ? "G" : "g";
             return g+"ender traitor";
         });
 	
